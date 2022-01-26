3e7a03bb0c6178d8fcf255ecd096ac50b61c18dd
Cole Trapnell <coletrap@uw.edu>
2022-01-26 16:51:35 -0500

    Update muscle Hooke example to just look at a simplified fast muscle trajectory

--- examples/muscle_mt.R
+++ examples/muscle_mt.R
@@ -56,13 +56,13 @@
 # saveRDS(meso_mt_cds, "../gap-notebook-ct-1/my_R_objects/meso_tbx16-like_18_24_cds.rds")
 # saveRDS(meso_mt_cds, "../gap-notebook-ct-1/my_R_objects/meso_tbx16-like_18_24_proj_cds.rds")
 
-meso_mt_cds <- readRDS("../gap-notebook-ct-1/my_R_objects/meso_tbx16-like_18_24_proj_cds.rds")
+meso_mt_cds <- readRDS("examples/R_objects/meso_tbx16-like_18_24_proj_cds.rds")
 
-meso_comb_cds <- combine_cds(list(meso_18_24_cds, meso_mt_cds), keep_reduced_dims = T, cell_names_unique = T)
+#meso_comb_cds <- combine_cds(list(meso_18_24_cds, meso_mt_cds), keep_reduced_dims = T, cell_names_unique = T)
 #saveRDS(meso_comb_cds, "../gap-notebook-ct-1/my_R_objects/meso_comb_18_24_proj_cds.rds")
 
-plot_cells(meso_comb_cds, color_cells_by = "cluster")
-plot_cells(meso_comb_cds, color_cells_by = "gene_target")
+#plot_cells(meso_comb_cds, color_cells_by = "cluster")
+#plot_cells(meso_comb_cds, color_cells_by = "gene_target")
 
 # ---------------------------------------------------------------------------
 
@@ -76,7 +76,7 @@ meso_mt_ccs = new_cell_count_set(meso_mt_cds,
 
 meso_mt_ccm = new_cell_count_model(meso_mt_ccs,
                                    model_formula_str = "~ as.factor(timepoint) + gene_target",
-                                   whitelist = total_green_edges,
+                                   #whitelist = total_green_edges,
                                    base_penalty = 5)
 
 plot(meso_mt_ccm@best_model, output="corrplot")
@@ -92,9 +92,9 @@ cond_mt_24_ctrlinj_v_tbx16_tbl = compare_abundances(meso_mt_ccm, time_24_ctrlinj
 cond_mt_18_24_ctrlinj_tbl = compare_abundances(meso_mt_ccm, time_18_ctrlinj, time_24_ctrlinj)
 
 
-plot_contrast(meso_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, scale_shifts_by = "receiver")
-plot_contrast(meso_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, scale_shifts_by = "sender")
-plot_contrast(meso_mt_ccm, cond_mt_24_ctrlinj_v_tbx16_tbl)
+plot_contrast(meso_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, scale_shifts_by = "receiver", p_value_thresh = 0.05)
+plot_contrast(meso_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, scale_shifts_by = "sender", p_value_thresh = 0.05)
+plot_contrast(meso_mt_ccm, cond_mt_24_ctrlinj_v_tbx16_tbl, p_value_thresh = 0.05)
 
 colData(meso_mt_cds)$umap_1 = reducedDims(meso_mt_cds)[["UMAP"]][,1]
 colData(meso_mt_cds)$umap_2 = reducedDims(meso_mt_cds)[["UMAP"]][,2]
@@ -120,6 +120,65 @@ sp = plot_shortest_path(meso_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, "1", "4",
                         plot_sp = T, plot_weights = F, sum_weights = T)
 sp
 # ggsave(sp, filename = "examples/muscle_time_plots/shortest_path_18_ctrlinj_tbx16_cluster2_to_4.png")
+
+# Fast muscle alone
+
+selected_cell_types = c("fast-committed myocyte (fusing)",
+                        "mature fast muscle 1",
+                        #"mature fast muscle 2",
+                        #"mature fast muscle 3",
+                        #"mature fast muscle 4",
+                        #"mature fast muscle 5",
+                        #"mature fast muscle 6",
+                        "fast-committed myocyte (pre-fusion)",
+                        "mesodermal progenitor cells (contains PSM)",
+                        #"satellite cells",
+                        "myoblast")
+fast_muscle_cds = meso_mt_cds[,colData(meso_mt_cds)$cell_type_sub %in% selected_cell_types]
+colData(fast_muscle_cds)$timepoint_num = as.numeric(colData(fast_muscle_cds)$timepoint)
+
+fast_muscle_cds = reduce_dimension(fast_muscle_cds)
+#fast_muscle_cds = cluster_cells(fast_muscle_cds)
+#plot_cells(fast_muscle_cds, color_cells_by="cluster")
+
+plot_cells(fast_muscle_cds, color_cells_by="cell_type_sub")
+
+
+fast_mt_ccs = new_cell_count_set(fast_muscle_cds,
+                                 sample_group = "embryo",
+                                 cell_group = "cell_type_sub")
+
+fast_mt_ccm = new_cell_count_model(fast_mt_ccs,
+                                   model_formula_str = "~ timepoint + gene_target",
+                                   #whitelist = total_green_edges,
+                                   base_penalty = 5)
+
+#fast_mt_ccm = select_model(fast_mt_ccm, criterion="StARS", sparsity_factor=2)
+
+plot(fast_mt_ccm@best_model, output="corrplot")
+
+time_18_ctrlinj = estimate_abundances(fast_mt_ccm, data.frame(timepoint="18", gene_target="ctrl-inj"))
+time_18_tbx16 = estimate_abundances(fast_mt_ccm, data.frame(timepoint="18", gene_target="tbx16"))
+time_24_ctrlinj = estimate_abundances(fast_mt_ccm, data.frame(timepoint="24", gene_target="ctrl-inj"))
+time_24_tbx16 = estimate_abundances(fast_mt_ccm, data.frame(timepoint="24", gene_target="tbx16"))
+
+
+cond_mt_18_ctrlinj_v_tbx16_tbl = compare_abundances(fast_mt_ccm, time_18_ctrlinj, time_18_tbx16)
+cond_mt_24_ctrlinj_v_tbx16_tbl = compare_abundances(fast_mt_ccm, time_24_ctrlinj, time_24_tbx16)
+cond_mt_18_24_ctrlinj_tbl = compare_abundances(fast_mt_ccm, time_18_ctrlinj, time_24_ctrlinj)
+
+
+plot_contrast(fast_mt_ccm, cond_mt_18_24_ctrlinj_tbl, p_value_thresh = 1)
+
+plot_contrast(fast_mt_ccm, cond_mt_18_24_ctrlinj_tbl, p_value_thresh = 1, plot_labels="none") +
+  ggsave("fast_muscle_wt_18_24.png", width=7, height=6)
+
+plot_contrast(fast_mt_ccm, cond_mt_24_ctrlinj_v_tbx16_tbl, p_value_thresh = 1, plot_labels="none") +
+  ggsave("fast_muscle_tbx16_18_24.png", width=7, height=6)
+
+plot_contrast(fast_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, scale_shifts_by = "receiver", p_value_thresh = 1)
+plot_contrast(fast_mt_ccm, cond_mt_18_ctrlinj_v_tbx16_tbl, scale_shifts_by = "sender", p_value_thresh = 0.05)
+plot_contrast(fast_mt_ccm, cond_mt_24_ctrlinj_v_tbx16_tbl, p_value_thresh = 1)
 
 
 
