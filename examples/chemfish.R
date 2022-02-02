2105b57142be75623551de8da3521e29178dbe04
maddyduran <maddyduran@gmail.com>
2022-02-02 11:20:22 -0800

    update chemfish/organoids

--- examples/chemfish.R
+++ examples/chemfish.R
@@ -21,18 +21,15 @@ if (!file.exists("R_objects/cds_metadata_proj_eliza.RDS")) {
 
 chemfish_cds = readRDS("R_objects/cds_metadata_proj_eliza.RDS")
 
-# colData(chemfish_cds)$drug %>% unique()
-# [1] "DMSO" "CP"   "EtOH" "SB" 
-
 # together --------------------------------------------------------------------
 chemfish_cds = chemfish_cds[,!is.na(colData(chemfish_cds)$major_group)]
 chemfish_cds = chemfish_cds[,!is.na(colData(chemfish_cds)$cell_type_broad)]
 cf_ccs = new_cell_count_set(chemfish_cds,
                             sample_group = "embryo",
-                            cell_group = "cell_type_broad")
+                            cell_group = "cell_type_sub")
 
 cf_ccm = new_cell_count_model(cf_ccs, 
-                              model_formula_str = "~ drug + splines::ns(timepoint, df=3) ")
+                              model_formula_str = "~ splines::ns(timepoint, df=3) + drug")
 
 
 cond_sb = estimate_abundances(cf_ccm, tibble::tibble(drug="SB", timepoint = "24"))
@@ -57,72 +54,80 @@ plot_contrast(cf_ccm,
               q_value_thresh = 0.05) 
 
 
+cond_cp_36 = estimate_abundances(cf_ccm, tibble::tibble(drug="CP", timepoint = "36"))
+cond_etoh_36 = estimate_abundances(cf_ccm, tibble::tibble(drug="EtOH", timepoint = "36"))
+cond_cp_vs_etoh_tbl_36 = compare_abundances(cf_ccm, cond_etoh_36, cond_cp_36)
+
+cond_cp_48 = estimate_abundances(cf_ccm, tibble::tibble(drug="CP", timepoint = "48"))
+cond_etoh_48 = estimate_abundances(cf_ccm, tibble::tibble(drug="EtOH", timepoint = "48"))
+cond_cp_vs_etoh_tbl_48 = compare_abundances(cf_ccm, cond_etoh_48, cond_cp_48)
 
 
+cond_cp_72 = estimate_abundances(cf_ccm, tibble::tibble(drug="CP", timepoint = "72"))
+cond_etoh_72 = estimate_abundances(cf_ccm, tibble::tibble(drug="EtOH", timepoint = "72"))
+cond_cp_vs_etoh_tbl_72 = compare_abundances(cf_ccm, cond_etoh_72, cond_cp_72)
 
-# figure out how to do the normalization? 
-# normalize by timepoint + drug ? 
-
-# SB --------------------------------------------------------------------------
-sb_cds = chemfish_cds[,colData(chemfish_cds)$drug %in% c("SB", "DMSO")]
-sb_cds = sb_cds[, !is.na(colData(sb_cds)$cell_type_broad)]
-sb_ccs = new_cell_count_set(sb_cds,
-                            sample_group = "embryo",
-                            cell_group = "cell_type_broad")
+rbind(cond_cp_vs_etoh_tbl_36,
+      cond_cp_vs_etoh_tbl_48,
+      cond_cp_vs_etoh_tbl_72) %>% 
+  filter(cell_group == "pronephric podocyte")
 
 
-sb_ccm = new_cell_count_model(sb_ccs, 
-                              model_formula_str = "~ drug + as.factor(timepoint)")
-
-# 24, 36, 60
-cond_sb = estimate_abundances(sb_ccm, tibble::tibble(drug="SB", timepoint ="60"))
-cond_dmso = estimate_abundances(sb_ccm, tibble::tibble(drug="DMSO", timepoint ="60"))
-
-cond_sb_vs_dmso_tbl = compare_abundances(sb_ccm, cond_dmso, cond_sb)
-
-cond_sb_vs_dmso_tbl %>% 
-  filter(delta_q_value < 0.05) %>% 
-  pull(cell_group)
-
-
-plot_contrast(sb_ccm, cond_sb_vs_dmso_tbl, 
-              scale_shifts_by="none", 
-              plot_labels = "significant", 
-              plot_edges = F, 
-              q_value_thresh = 0.05)
-
-
-
-# CP --------------------------------------------------------------------------
-cp_cds = chemfish_cds[,colData(chemfish_cds)$drug %in%  c("CP", "EtOH")]
-cp_cds = cp_cds[, !is.na(colData(cp_cds)$cell_type_broad)]
-cp_ccs = new_cell_count_set(cp_cds,
-                            sample_group = "embryo",
-                            cell_group = "cell_type_broad")
-
-cp_ccm = new_cell_count_model(cp_ccs, 
-                              model_formula_str = "~ drug + as.factor(timepoint)")
-
-# 36, 48, 72
-cond_cp = estimate_abundances(cp_ccm, tibble::tibble(drug="CP", timepoint = "36"))
-cond_etoh = estimate_abundances(cp_ccm, tibble::tibble(drug="EtOH", timepoint = "36"))
-
-cond_cp_vs_etoh_tbl = compare_abundances(cp_ccm, cond_etoh, cond_cp)
-
-cond_cp_vs_etoh_tbl %>% 
-  filter(delta_q_value < 0.05) %>% 
-  pull(cell_group)
-
-plot_contrast(cp_ccm, 
-              cond_cp_vs_etoh_tbl, 
-              scale_shifts_by="none", 
+plot_contrast(cf_ccm,
+              cond_cp_vs_etoh_tbl_36,
+              scale_shifts_by="none",
               plot_labels = "significant",
-              plot_edges = T, 
+              plot_edges = T,
               q_value_thresh = 0.05)
 
 
+cond_cp_48 = estimate_abundances(cp_ccm, tibble::tibble(drug="CP", timepoint = "48"))
+cond_etoh_48 = estimate_abundances(cp_ccm, tibble::tibble(drug="EtOH", timepoint = "48"))
+
+cond_cp_vs_etoh_tbl_48 = compare_abundances(cp_ccm, cond_etoh_48, cond_cp_48)
+
+plot_contrast(cf_ccm,
+              cond_cp_vs_etoh_tbl_48,
+              scale_shifts_by="none",
+              plot_labels = "significant",
+              plot_edges = T,
+              q_value_thresh = 0.05)
+
+cond_cp_72 = estimate_abundances(cp_ccm, tibble::tibble(drug="CP", timepoint = "72"))
+cond_etoh_72 = estimate_abundances(cp_ccm, tibble::tibble(drug="EtOH", timepoint = "72"))
+
+cond_cp_vs_etoh_tbl_72 = compare_abundances(cp_ccm, cond_etoh_72, cond_cp_72)
+
+plot_contrast(cf_ccm,
+              cond_cp_vs_etoh_tbl_72,
+              scale_shifts_by="none",
+              plot_labels = "significant",
+              plot_edges = T,
+              q_value_thresh = 0.05)
 
 
+rbind(cond_cp_vs_etoh_tbl_36,
+      cond_cp_vs_etoh_tbl_48,
+      cond_cp_vs_etoh_tbl_72) %>% 
+  filter(cell_group == "pronephric podocyte")
 
 
+plot_sub_contrast(cf_ccm, 
+                  cond_b_vs_a_tbl = cond_cp_vs_etoh_tbl_36, 
+                  cell_group = "cell_type_sub",
+                  scale_shifts_by="none", 
+                  plot_labels = "significant", 
+                  plot_edges = F, 
+                  q_value_thresh = 0.05)
 
+plot_sub_contrast(cf_ccm, 
+                  cond_b_vs_a_tbl = cond_cp_vs_etoh_tbl_36, 
+                  cell_group = "cell_type_sub",
+                  select_group = "CNS",
+                  scale_shifts_by="none", 
+                  plot_labels = "significant", 
+                  plot_edges = F, 
+                  q_value_thresh = 0.05)
+
+
+# filter(cell_group == "xanthophore") 
