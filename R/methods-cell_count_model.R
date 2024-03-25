b8129c2d1b946dcb5987e2b298642f7c5ac3f2de
Madeleine Duran <maddyduran@gmail.com>
2024-03-25 14:48:16 -0700

    subset cds coldata, make tibble

--- R/methods-cell_count_model.R
+++ R/methods-cell_count_model.R
@@ -102,7 +102,7 @@ subset_ccs = function(ccs, ...) {
   sub_ccs = ccs[cell_groups, samples]
   sub_ccs@metadata$cell_group_assignments = cell_group_assignments
   sub_ccs@cds = sub_ccs@cds[, colData(sub_ccs@cds)$cell %in% filtered_cds_coldata$cell]
-  sub_ccs@cds_coldata = filtered_cds_coldata
+  sub_ccs@cds_coldata = filtered_cds_coldata %>% as_tibble()
   
 
   return(sub_ccs)
