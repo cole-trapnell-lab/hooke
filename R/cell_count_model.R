21c18dc1a88f3df8d0e678ab7d8250e4fcdbdc3b
Madeleine Duran <maddyduran@gmail.com>
2024-05-14 11:02:36 -0700

    updating namespace

--- R/cell_count_model.R
+++ R/cell_count_model.R
@@ -421,6 +421,7 @@ empty_ccs <- function() {
 #' @importFrom PLNmodels PLNnetwork
 #' @importFrom PLNmodels getBestModel
 #' @importFrom PLNmodels getModel
+#' @import spines
 #' @export
 new_cell_count_model <- function(ccs,
                                  main_model_formula_str,
