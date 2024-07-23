0c06ed19e9feb0a0629c2fd1e67d3c0985595a91
rfriedman22 <ryanfriedman22@gmail.com>
2024-07-23 15:05:58 -0700

    Don't filter cells with zeros, filter metacells with zeros

--- R/deg.R
+++ R/deg.R
@@ -67,13 +67,6 @@ pseudobulk_ccs_for_states <- function(ccs,
   }
   else {
     sub_cds = ccs@cds[gene_group_df$id,]
-
-    sub_cds = sub_cds[, Matrix::colSums(counts(sub_cds)) > 0]
-    cell_group_df = cell_group_df %>%
-      filter(rowname %in% colnames(sub_cds))
-    agg_coldata = agg_coldata %>%
-      filter(pseudobulk_id %in% cell_group_df$pseudobulk_id)
-
     agg_expr_mat = monocle3::aggregate_gene_expression(
       sub_cds,
       cell_group_df = cell_group_df,
@@ -84,6 +77,10 @@ pseudobulk_ccs_for_states <- function(ccs,
       cell_agg_fun = cell_agg_fun,
       gene_agg_fun = gene_agg_fun
     )
+
+    agg_expr_mat = agg_expr_mat[,Matrix::colSums(agg_expr_mat) > 0]
+    agg_coldata = agg_coldata %>%
+      filter(pseudobulk_id %in% colnames(agg_expr_mat))
 
     agg_rowdata = agg_rowdata[row.names(agg_expr_mat),]
   }
